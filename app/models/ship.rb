0b11944cd93804eedb4773da060f435b444a3084
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-25 05:56:41 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,7 +2,7 @@ class Ship
   attr_accessor :name, :type, :booty
   @@all = []
 
-  def initialize(name, type, booty)
+  def initialize(params)
     @name = params[:name]
     @type = params[:type]
     @booty = params[:booty]
